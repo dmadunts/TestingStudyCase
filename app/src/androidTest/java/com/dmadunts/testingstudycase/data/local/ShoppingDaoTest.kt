6930fb1eb1205b89607b9a46cf8fc6265a76a28a
dmadunts <madunts.d@gmail.com>
2022-02-22 17:51:13 +0500

    Adding tests for ShoppingDao

---
+++ app/src/androidTest/java/com/dmadunts/testingstudycase/data/local/ShoppingDaoTest.kt
@@ -0,0 +1,80 @@
+package com.dmadunts.testingstudycase.data.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.dmadunts.testingstudycase.getOrAwaitValue
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class ShoppingDaoTest {
+    private lateinit var database: ShoppingItemDatabase
+    private lateinit var dao: ShoppingDao
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ShoppingItemDatabase::class.java
+        ).allowMainThreadQueries()
+            .build()
+
+        dao = database.shoppingDao()
+    }
+
+    @After
+    fun teardown() {
+        database.close()
+    }
+
+    @Test
+    fun insertShoppingItem() = runTest {
+        val shoppingItem = ShoppingItem("name", 1f, 1, 1)
+        dao.insertShoppingItem(shoppingItem)
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
+        assertThat(allShoppingItems).contains(shoppingItem)
+    }
+
+    @Test
+    fun deleteShoppingItem() = runTest {
+        val shoppingItem = ShoppingItem("name", 1f, 1, 1)
+        dao.insertShoppingItem(shoppingItem)
+        dao.deleteShoppingItem(shoppingItem)
+        val allShoppingItems = dao.observeAllShoppingItems().getOrAwaitValue()
+        assertThat(allShoppingItems).doesNotContain(shoppingItem)
+    }
+
+    @Test
+    fun observeTotalPrice() = runTest {
+        val items = arrayListOf(
+            ShoppingItem("name", 100f, 5, 1),
+            ShoppingItem("name2", 50f, 3, 2),
+            ShoppingItem("name3", 33f, 10, 3)
+        )
+
+        items.forEach { item ->
+            dao.insertShoppingItem(item)
+        }
+
+        var expectedTotalPrice = 0f
+        items.forEach { item ->
+            expectedTotalPrice += item.price * item.amount
+        }
+        val actualTotalPrice = dao.observeTotalPrice().getOrAwaitValue()
+        assertThat(expectedTotalPrice).isEqualTo(actualTotalPrice)
+    }
+}
