fc77e2fdf2e89aa3d55aa3f16b8b2deabde8bcdf
dmadunts <madunts.d@gmail.com>
2022-02-22 19:17:41 +0500

    Add DaggerHilt AppModule.kt

---
+++ app/src/main/java/com/dmadunts/testingstudycase/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.dmadunts.testingstudycase.di
+
+import android.content.Context
+import androidx.room.Room
+import com.dmadunts.testingstudycase.data.local.ShoppingItemDatabase
+import com.dmadunts.testingstudycase.data.remote.apis.PixabayApi
+import com.dmadunts.testingstudycase.utils.Constants.BASE_URL
+import com.dmadunts.testingstudycase.utils.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, ShoppingItemDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDao(database: ShoppingItemDatabase) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providePixabayApi(): PixabayApi {
+        return Retrofit.Builder().baseUrl(BASE_URL).build()
+            .create(PixabayApi::class.java)
+    }
+}
---
+++ app/src/main/java/com/dmadunts/testingstudycase/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.dmadunts.testingstudycase.utils
+
+object Constants {
+    const val DATABASE_NAME = "shopping_db"
+    const val BASE_URL = "https://pixabay.com"
+}
