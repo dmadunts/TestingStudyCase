9467a21d665f44d44d1a3d1b2b56eed30f91ba7c
dmadunts <madunts.d@gmail.com>
2022-03-01 15:33:24 +0500

    Add HiltTestRunner.kt, TestAppModule.kt

--- app/src/androidTest/java/com/dmadunts/testingstudycase/data/local/ShoppingDaoTest.kt
+++ app/src/androidTest/java/com/dmadunts/testingstudycase/data/local/ShoppingDaoTest.kt
@@ -1,38 +1,40 @@
 package com.dmadunts.testingstudycase.data.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.dmadunts.testingstudycase.utils.getOrAwaitValue
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
 @MediumTest
+@HiltAndroidTest
 class ShoppingDaoTest {
-    private lateinit var database: ShoppingItemDatabase
+
+    @Inject
+    @Named("test_db")
+    lateinit var database: ShoppingItemDatabase
+
     private lateinit var dao: ShoppingDao
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+
     @Before
     fun setUp() {
-        database = Room.inMemoryDatabaseBuilder(
-            ApplicationProvider.getApplicationContext(),
-            ShoppingItemDatabase::class.java
-        ).allowMainThreadQueries()
-            .build()
-
+        hiltRule.inject()
         dao = database.shoppingDao()
     }
 
